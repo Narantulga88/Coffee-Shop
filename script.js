cc47f50e2ff7d40976076fbe98b1749eeb33c89a
Narantulga88 <khurelchuluunnarantulga@gmail.com>
2025-12-05 11:41:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// Mobile menu toggle
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
+
+// Close menu when the close nav link is clicked
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click())
+})
+
+// Initialize Swiper
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+});
